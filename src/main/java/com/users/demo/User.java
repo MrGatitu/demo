9b0eb50990054f5417f2244672e1643f92a4954f
MrGatitu <mrgatituh@gmail.com>
2024-07-20 19:43:01 +0300

    Had an Error with package name. Resolved by restarting the editor.

---
+++ src/main/java/com/users/demo/User.java
@@ -0,0 +1,20 @@
+package com.users.demo;
+
+public class User {
+
+    int id;
+    String name;
+    String email;
+    String hashedPass;
+
+    public User(int id, String name, String email, String hashedPass){
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.hashedPass = hashedPass;
+
+    }
+
+    
+
+}
